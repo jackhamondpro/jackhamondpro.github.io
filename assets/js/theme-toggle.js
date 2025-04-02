5ecc48304cbcc1f11968e90313baac8ddd5e8f4c
jackhamondpro <jackhamond@engineer.com>
2025-04-02 14:38:22 -0500

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,14 +1,14 @@
-// Check the stored theme preference on page load
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggleButton = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme'); // Get the saved theme from localStorage
 
-    // If a theme is saved in localStorage, apply it
+    // Check if the currentTheme exists, if so, apply it
     if (currentTheme) {
         document.body.classList.add(currentTheme);
     } else {
         // If no theme is saved, default to dark mode
         document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode'); // Save dark mode preference as default
     }
 
     // Toggle the theme and save the preference in localStorage
