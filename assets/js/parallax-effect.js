920ef6829cd7d92006329deb9ffef1197770f2c4
jackhamondpro <jackhamond@engineer.com>
2025-04-06 22:04:39 -0500

    Delete assets/js/parallax-effect.js

--- assets/js/parallax-effect.js
+++
@@ -1,6 +0,0 @@
-// Use a simple parallax effect for background
-document.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    const background = document.querySelector('.hero');
-    background.style.transform = 'translateY(' + scrollPosition * 0.2 + 'px)';
-});
